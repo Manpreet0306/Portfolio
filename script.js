db801e7eb15577db775d569bccd9d9abee04f771
manpreet singh <manpreet.singh.cse.2021@miet.ac.in>
2024-10-02 13:40:09 +0530

    update1.2

--- script.js
+++ script.js
@@ -1,30 +1,155 @@
 const div1 = document.querySelector('.nav-logo');
-    const div2 = document.querySelector('.slider');
+const div2 = document.querySelector('.slider');
 
-    div1.addEventListener('click', function() {
-        div2.classList.toggle('active');
-         // Toggle the 'active' class on click
-    });
-    div1.addEventListener('click', function() {
-        div1.classList.toggle('active1'); // Toggle the 'active' class on click
-    });
-    const btn1=document.querySelector('.btn-grp1');
-    btn1.addEventListener('click',function(){
-        window.location.href="https://github.com/Manpreet0306/Notepad.git";
-    });
-    const btn2=document.querySelector('.btn-grp2');
-    btn2.addEventListener('click',function(){
-        window.location.href="https://manpreet0306.github.io/QR_code_GN/";
-    });
-    const btn3=document.querySelector('.btn-grp3');
-    btn3.addEventListener('click',function(){
-        window.location.href="https://manpreet0306.github.io/Quiz-App/";
-    });
-    const btn4=document.querySelector('.btn-grp4');
-    btn4.addEventListener('click',function(){
-        window.location.href=" https://manpreet0306.github.io/Password_GEN/";
-    });
-    const hireme=document.getElementById('btn');
-    hireme.addEventListener('click',function(){
-        window.location.href="#contactme";
-    });
+div1.addEventListener('click', function () {
+    div2.classList.toggle('active');
+    // Toggle the 'active' class on click
+});
+div1.addEventListener('click', function () {
+    div1.classList.toggle('active1'); // Toggle the 'active' class on click
+});
+const btn1 = document.querySelector('.btn-grp1');
+btn1.addEventListener('click', function () {
+    window.location.href = "https://github.com/Manpreet0306/Notepad.git";
+});
+const btn2 = document.querySelector('.btn-grp2');
+btn2.addEventListener('click', function () {
+    window.location.href = "https://manpreet0306.github.io/QR_code_GN/";
+});
+const btn3 = document.querySelector('.btn-grp3');
+btn3.addEventListener('click', function () {
+    window.location.href = "https://manpreet0306.github.io/Quiz-App/";
+});
+const btn4 = document.querySelector('.btn-grp4');
+btn4.addEventListener('click', function () {
+    window.location.href = " https://manpreet0306.github.io/Password_GEN/";
+});
+
+const linkedin = document.getElementById('linkedin');
+linkedin.addEventListener('click', function () {
+    window.location.href = "https://www.linkedin.com/in/manpreet-singh-717920221";
+});
+const github = document.getElementById('github');
+github.addEventListener('click', function () {
+    window.location.href = "https://github.com/Manpreet0306";
+});
+const insta = document.getElementById('insta');
+insta.addEventListener('click', function () {
+    window.location.href = "https://www.instagram.com/_preet_gurjar_03?igsh=MThlZmMybTl2ankzMw==";
+});
+const twitter = document.getElementById('twit');
+twitter.addEventListener('click', function () {
+    window.location.href = "";
+});
+const msg = document.getElementById('message');
+msg.addEventListener('click', function () {
+    var recipient = "chokkarpreet@gmail.com";  // Set your email address here
+            var sub = "Inquiry from the Website";
+            var bod = "Hi, I would like to inquire about...";
+
+            // Construct the mailto URL
+            var mailtoLink = "mailto:" + recipient + "?subject=" + encodeURIComponent(sub) + "&body=" + encodeURIComponent(bod);
+            var tempLink = document.createElement("a");
+            tempLink.href = mailtoLink;
+
+            // Append the link to the body (it won't be visible)
+            document.body.appendChild(tempLink);
+
+            // Programmatically click the anchor link
+            tempLink.click();
+
+            // Remove the link after clicking
+            document.body.removeChild(tempLink);
+});
+
+const hireme = document.getElementById('btn');
+hireme.addEventListener('click', function () {
+    window.location.href = "#contactme";
+});
+
+const form = document.querySelector('form');
+const fullname = document.getElementById('Name');
+const email = document.getElementById('Email');
+const subject = document.getElementById('Subject');
+const message = document.getElementById('Message');
+
+function SendEmail() {
+    const bodymessage = `fullName: ${fullname.value}<br> Email: ${email.value}<br> subject: ${subject.value}<br> message: ${message.value}<br>`;
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "chokkarpreet@gmail.com",
+        Password: "7428039A1B3BB0778E7A5AF2806323D09CF3",
+        To: 'chokkarpreet@gmail.com',
+        From: email.value,
+        Subject: subject.value,
+        Body: bodymessage
+    }).then(
+        message => {
+            if (message == "OK") {
+                Swal.fire({
+                    title: "Success!",
+                    text: "Message is sent successfully!",
+                    icon: "success"
+                });
+            }
+        }
+    );
+}
+function checkinput() {
+    const items = document.querySelectorAll('.formfield');
+    for (const item of items) {
+
+        if (item.value == "") {
+
+            item.classList.add("error");
+
+            item.parentElement.classList.add("error");
+
+        }
+        if(items[1].value !==""){
+            checkmail();
+        }
+        items[1].addEventListener("keyup",()=>{
+            checkmail();
+        });
+
+        item.addEventListener("keyup", () => {
+
+            if (item.value != "") {
+
+                item.classList.remove("error");
+
+                item.parentElement.classList.remove("error");
+
+            }
+
+            else {
+
+                item.classList.add("error");
+
+                item.parentElement.classList.add("error");
+
+            }
+
+        });
+    }
+
+}
+function checkmail(){
+    const emailRegex=/^([a-z\d\-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+
+    if(!email.value.match(emailRegex)){
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+    }else{
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkinput();
+    if (!fullname.classList.contains("error") && !email.classList.contains ("error") && !subject.classList.contains("error") && !message.classList.contains("error")) {
+        SendEmail();
+    }
+})
